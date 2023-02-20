ae397308b6ef849dbb347f1536aff9883be953ee
negatively <mileniandi39@gmail.com>
2023-02-20 11:07:08 +0700

    edit inheritance

---
+++ Prodemi-TA1/src/com/milenian/Calculate.java
@@ -0,0 +1,29 @@
+package com.milenian;
+
+import com.milenian.bidang.*;
+import com.milenian.hitung.*;
+
+public class Calculate {
+	public static void main(String[] args) {
+		HitungBidang[] daftarBidang = {
+				new BelahKetupat(12,10),
+				new BelahKetupat(16.8, 40.4),
+				new BelahKetupat(88.88, 60),
+				new Lingkaran(50),
+				new Lingkaran(140),
+				new Lingkaran(4.20),
+				new Persegi(20),
+				new Persegi(25.4),
+				new Persegi(33.33),
+				new Segitiga(12,6),
+				new Segitiga(400, 82.8),
+				new Segitiga(70.70, 100.80)
+		};
+		
+		for(HitungBidang bd: daftarBidang) {
+			System.out.println(bd.luas());
+		}
+		
+	}
+
+}
---
+++ Prodemi-TA1/src/com/milenian/hitung/HitungBidang.java
@@ -0,0 +1,15 @@
+package com.milenian.hitung;
+
+public abstract class HitungBidang {
+	public double luasBidang;
+	public double kelilingBidang;
+	
+	public String luas() {
+		return "";
+	}
+	
+	public String keliling() {
+		return "";		
+	}
+
+}
